4149b9cf159ede305e30209a070b4feadb68e78f
Brandon <allen95brandon@gmail.com>
2025-01-21 18:40:14 -0800

    Finished ADC driver

--- ADC_driver/Core/Src/main.c
+++ ADC_driver/Core/Src/main.c
@@ -1,6 +1,22 @@
 #include "RccConfig.h"
 
+/**
+ * Static variables
+ */
+uint16_t vals[3] = {0, 0, 0};
+float device_temp;
+float external_temp;
+uint16_t test_output;
 
+float device_calc(uint16_t value){
+	return ( (value - 760)/2.5) + 25;
+}
+
+float external_calc(uint16_t value){
+//	float vref = 3.3; // ADC reference voltage
+//	float vout = ( ((float) value) / 4096.0) * vref; // Convert ADC value to voltage
+	return (float) (value - 500) / 10; // Convert voltage to temperature
+}
 void ADC_Init(){
 
 	//Enable the ADC clock from APB2 and GPIOA clock from AHB1
@@ -11,20 +27,20 @@ void ADC_Init(){
 	ADC->CCR |= (1 << 16);
 
 	//Set scan mode and resolution
-	ADC->CR1 |= (1 << 8); //Scan mode enabled
-	ADC->CR1 &= ~(3 << 24); //12 bit resolution
+	ADC1->CR1 |= (1 << 8); //Scan mode enabled
+	ADC1->CR1 &= ~(3 << 24); //12 bit resolution
 
 	//Setting continuous conversion, EOC, and data alignment
-	ADC->CR2 |= (1 << 1); //Setting continuous conversion
-	ADC->CR2 |= (1 << 10); //Overrun bit set at the end of each conversion.
-	ADC->CR2 &= ~(1 << 11); //Align data to the right
+	ADC1->CR2 |= (1 << 1); //Setting continuous conversion
+	ADC1->CR2 |= (1 << 10); //Overrun bit set at the end of each conversion.
+	ADC1->CR2 &= ~(1 << 11); //Align data to the right
 
 	//Configure sampling times for channels 1, 4, and 16 (latter is for temperature sensor).
-	ADC->SMPR2 &= ~((3<<3) | (3<<12)); // Set channel 1 to sampling time of 3 and channel 4 to sampling time of 3.
-	ADC->SMPR1 &= ~(3 << 18); //Min samp_time for temp_sens = 10us, 10us/47,62ns = 210 cycles required, so setting to 480.
+	ADC1->SMPR2 &= ~((3<<3) | (3<<12)); // Set channel 1 to sampling time of 3 and channel 4 to sampling time of 3.
+	ADC1->SMPR1 &= ~(3 << 18); //Min samp_time for temp_sens = 10us, 10us/47,62ns = 210 cycles required, so setting to 480.
 
 	//Set number of channels in conversion sequence (2 in this case)
-	ADC->SQR1 |= (2 << 20); //Number of channels in sequence.
+	ADC1->SQR1 |= (2 << 20); //Number of channels in sequence.
 
 	//Set respective GPIO PINs to Analog Mode.
 	GPIOA->MODER |= ((3 << 2) | (3 << 8)); //Set PA1 and PA4 to analog.
@@ -32,7 +48,7 @@ void ADC_Init(){
 
 void ADC_Enable(void){
 	//Enable ADC
-	ADC->CR2 |= (1 << 0);
+	ADC1->CR2 |= (1 << 0);
 	//Enable built-in temp sensor
 	ADC->CCR |= (1 << 23);
 
@@ -42,42 +58,72 @@ void ADC_Enable(void){
 }
 
 void ADC_Start(int channel){
-	ADC->SQR3 = 0;
-	ADC->SQR3 |= (channel << 0); // mAKE
+	ADC1->SQR3 = 0;
+	ADC1->SQR3 |= (channel << 0); // mAKE
 
-	ADC->SR = 0; //Clear status register
+	ADC1->SR = 0; //Clear status register
 
-	ADC->CR2 |= (1 << 30); //Start conversion
+	ADC1->CR2 |= (1 << 30); //Start conversion
 }
 
 void ADC_WaitForConversion(void){
-	while (!(ADC->SR & (1 <<1))); //Wait for EOC flag to set.
+	while (!(ADC1->SR & (1 <<1))); //Wait for EOC flag to set.
 }
 
 uint16_t ADC_GetVal(void){
-	return ADC->DR; //Return value
+	return ADC1->DR; //Return value
 }
 
 void ADC_Disable(void){
-	ADC->CR &= ~(1<<0); // Disable ADC
+	ADC1->CR2 &= ~(1<<0); // Disable ADC
 }
 
+void ADC_Sequential(){
+	ADC1->SQR3 = 0;
+	ADC1->SQR3 |= (1<<0); //Channel 1 first
+	ADC1->SQR3 |= (4<<5); //Channel 2 second
+	ADC1->SQR3 |= (16<<10); //Channel 16 (embedded temp sensor 3rd)
 
-uint16_t vals[2] = {0,0};
+	ADC1->SR = 0; //Clear SR
+	ADC1->CR2 |= (1 << 30); //Start conversion
+
+	//Poll for each conversion
+	for(int i = 0; i < 3; ){
+		if (ADC1->SR & (1 <<1)){ //EOC set
+			vals[i] = ADC1->DR;
+			i++;
+		}
+	}
+	test_output = vals[0];
+	external_temp = external_calc(vals[1]);
+	device_temp = device_calc(vals[2]);
+}
+
 
 int main(){
 	SysClockConfig();
 	ADC_Init();
 	ADC_Enable();
 
-	while(1){
-		ADC_Start(1);
-		ADC_WaitForConv();
-		ADC_VAL[0] = ADC_GetVal();
+	//Manual conversion method (scan mode made redundant)
+//	while(1){
+//		ADC_Start(1);
+//		ADC_WaitForConversion();
+//		test_output = ADC_GetVal();
+//
+//
+//		ADC_Start(4);
+//		ADC_WaitForConversion();
+//		external_temp = external_calc(ADC_GetVal());
+//
+//		ADC_Start(16);
+//		ADC_WaitForConversion();
+//		device_temp = device_calc(ADC_GetVal());
+//	}
 
-		ADC_Start(4);
-		ADC_WaitForConv();
-		ADC_VAL[1] = ADC_GetVal();
+	//Sequential order used
+	while(1){
+		ADC_Sequential();
 	}
 
 }
